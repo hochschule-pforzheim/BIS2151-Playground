e5944dab5a221c23a8448ef456b9a47be071f120
Murungwe <mjaybless@gmail.com>
2021-05-04 11:03:12 +0200

    this is wrong

--- JavaPlayground/src/javaplayground/JavaPlayground.java
+++ JavaPlayground/src/javaplayground/JavaPlayground.java
@@ -15,7 +15,7 @@ public class JavaPlayground
      */
     public static void main(String[] args)
     {
-        System.out.println("Helo World."); 
+        System.out.println("This is wrong."); 
     }
 
 }
