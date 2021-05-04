5632251814973df11573dfed1a4b8a3d20b49876
Jonathan Hinkel <asder0815@gmail.com>
2021-05-04 11:00:27 +0200

    Added another sentence.

--- JavaPlayground/src/javaplayground/JavaPlayground.java
+++ JavaPlayground/src/javaplayground/JavaPlayground.java
@@ -16,6 +16,7 @@ public class JavaPlayground
     public static void main(String[] args)
     {
         System.out.println("Helo World."); 
+        System.out.println("This is hwo you use git."); 
     }
 
 }
