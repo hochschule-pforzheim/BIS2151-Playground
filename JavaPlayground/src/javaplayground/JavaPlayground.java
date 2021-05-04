27c9d6ada5a0ab58b2ed95364b90fc148ffa6e9f
Andreas Schneider <mail@ainz.one>
2021-05-04 10:47:22 +0200

    Class JavaPlayground

---
+++ JavaPlayground/src/javaplayground/JavaPlayground.java
@@ -0,0 +1,21 @@
+package javaplayground;
+
+/**
+ * This class if for testing git and GitHub.
+ *
+ * @author Andreas Schneider
+ */
+public class JavaPlayground
+{
+
+    /**
+     * Simple main method.
+     *
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+    }
+
+}
